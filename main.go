92973dda73931d357e0d9166f0e2c4b7c2f5610e
jim-ww <jim.w2610@proton.me>
2026-06-24 18:22:37 +0200

    refactor: simplify code

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"time"
@@ -10,13 +11,11 @@ import (
 	"github.com/0xAX/notificator"
 )
 
-const appName = "pomodoro"
-
 func main() {
 	flag.Usage = func() {
-		fmt.Printf("Usage:\n%s [flags] work_duration pause_duration\n\n", appName)
+		fmt.Println("Usage: pomodoro [flags] work_duration pause_duration\n")
 		flag.PrintDefaults()
-		fmt.Printf("\nExamples:\n\t%s 25 5\n\t%s 25m 5m\n\t%s 45m 10m\n\t%s 1h 15m\n\t%s 90s 15s\n\n", appName, appName, appName, appName, appName)
+		fmt.Printf("\nExamples:\n\tpomodoro 25 5\n\tpomodoro 25m 5m\n\tpomodoro 45m 10m\n\tpomodoro 1h 15m\n\tpomodoro 90s 15s\n\n")
 	}
 	workText := flag.String("workText", "Back to work!", "work notification text")
 	pauseText := flag.String("pauseText", "Time for a break!", "break notification text")
@@ -24,14 +23,12 @@ func main() {
 	pauseIconPath := flag.String("pauseIconPath", "", "break notification icon")
 	defaultIconPath := flag.String("defaultIconPath", "", "default notification icon")
 	flag.Parse()
-
 	if flag.NArg() != 2 {
 		flag.Usage()
 		os.Exit(1)
 	}
 
 	args := flag.Args()
-
 	parseDuration := func(s string) time.Duration {
 		duration, err := time.ParseDuration(s)
 		if err == nil {
@@ -41,76 +38,27 @@ func main() {
 		if err == nil {
 			return time.Minute * time.Duration(minutes)
 		}
-		fmt.Printf("failed to parse duration: %s\n\n", err)
 		flag.Usage()
-		os.Exit(1)
+		log.Fatalf("\n\nfailed to parse duration: %s", err)
 		return 0
 	}
 
 	workDuration := parseDuration(args[0])
 	pauseDuration := parseDuration(args[1])
-
 	if workDuration <= 0 || pauseDuration <= 0 {
-		fmt.Println("work_duration and pause_duration must be greater than 0")
-		os.Exit(1)
+		log.Fatal("work_duration and pause_duration must be greater than 0")
 	}
 
-	if err := startPomodoro(config{
-		WorkDuration:  workDuration,
-		PauseDuration: pauseDuration,
-		WorkText:      *workText,
-		PauseText:     *pauseText,
-		DefaultIcon:   *defaultIconPath,
-		WorkIcon:      *workIconPath,
-		PauseIcon:     *pauseIconPath,
-	}); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-}
-
-type config struct {
-	WorkDuration  time.Duration
-	PauseDuration time.Duration
-
-	WorkText  string
-	PauseText string
-
-	DefaultIcon string
-	WorkIcon    string
-	PauseIcon   string
-}
-
-func startPomodoro(cfg config) error {
-	notif := notificator.New(notificator.Options{AppName: appName, DefaultIcon: cfg.DefaultIcon})
-	pushWork := func() error {
-		return notif.Push(cfg.WorkText, "", cfg.WorkIcon, notificator.UR_NORMAL)
-	}
-	pushPause := func() error {
-		return notif.Push(cfg.PauseText, "", cfg.PauseIcon, notificator.UR_NORMAL)
-	}
-
-	if err := pushWork(); err != nil {
-		return fmt.Errorf("push notification: %w", err)
-	}
-
-	working := true
-	ticker := time.NewTicker(cfg.WorkDuration)
-	defer ticker.Stop()
-
-	for range ticker.C {
-		if working {
-			if err := pushPause(); err != nil {
-				return fmt.Errorf("push notification: %w", err)
-			}
-			ticker.Reset(cfg.PauseDuration)
-		} else {
-			if err := pushWork(); err != nil {
-				return fmt.Errorf("push notification: %w", err)
-			}
-			ticker.Reset(cfg.WorkDuration)
+	notif := notificator.New(notificator.Options{AppName: "pomodoro", DefaultIcon: *defaultIconPath})
+	notify := func(text, iconPath string) {
+		if err := notif.Push(text, "", iconPath, notificator.UR_NORMAL); err != nil {
+			log.Fatal(err)
 		}
-		working = !working
 	}
-	return nil
+	for {
+		notify(*workText, *workIconPath)
+		time.Sleep(workDuration)
+		notify(*pauseText, *pauseIconPath)
+		time.Sleep(pauseDuration)
+	}
 }
